19e23c24452a198c088f8de02d3fe9d2dbe69481
号长 <1136560037@qq.com>
2019-08-01 17:45:59 +0800

    8.1.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <el-button type='success'>成功按钮</el-button>
   </div>
 </template>
 
